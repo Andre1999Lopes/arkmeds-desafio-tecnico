18405a2f6a9930872725c69a24f346d172426e12
Andre1999Lopes <andrelopesbr1999@gmail.com>
2024-08-24 14:10:00 -0300

    fix: changed 'Ride' field name to 'rides'

---
+++ src/main/prisma/migrations/20240824164436_table_creation/migration.sql
@@ -0,0 +1,69 @@
+-- CreateTable
+CREATE TABLE "Passenger" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "sex" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Passenger_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Driver" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "sex" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "licenseNumber" TEXT NOT NULL,
+    "vehiclePlate" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Driver_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Ride" (
+    "id" UUID NOT NULL,
+    "passengerId" UUID NOT NULL,
+    "driverId" UUID NOT NULL,
+    "fare" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Ride_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Passenger_cpf_key" ON "Passenger"("cpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Passenger_email_key" ON "Passenger"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Driver_cpf_key" ON "Driver"("cpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Driver_email_key" ON "Driver"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Driver_licenseNumber_key" ON "Driver"("licenseNumber");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Driver_vehiclePlate_key" ON "Driver"("vehiclePlate");
+
+-- AddForeignKey
+ALTER TABLE "Ride" ADD CONSTRAINT "Ride_passengerId_fkey" FOREIGN KEY ("passengerId") REFERENCES "Passenger"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Ride" ADD CONSTRAINT "Ride_driverId_fkey" FOREIGN KEY ("driverId") REFERENCES "Driver"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
