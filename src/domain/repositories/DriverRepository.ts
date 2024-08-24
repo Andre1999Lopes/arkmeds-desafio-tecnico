b2a79ec1b2dceb9bcdf28e95fda9eb3792398ba1
Andre1999Lopes <andrelopesbr1999@gmail.com>
2024-08-24 13:16:09 -0300

    feat: added methods on passenger and driver's repositories

--- src/domain/repositories/DriverRepository.ts
+++ src/domain/repositories/DriverRepository.ts
@@ -3,6 +3,7 @@ import { Driver } from '../entities/Driver';
 export interface DriverRepository {
 	create(driver: Driver): Promise<Driver>;
 	findById(driverId: string): Promise<Driver>;
+	findByCpf(driverCpf: string): Promise<Driver>;
 	findAll(): Promise<Driver[]>;
 	update(driver: Driver): Promise<Driver>;
 	delete(driverId: string): Promise<void>;
