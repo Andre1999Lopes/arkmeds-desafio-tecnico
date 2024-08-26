623be3fce47b7ceb788ba10540d27871bdea395f
Andre1999Lopes <andrelopesbr1999@gmail.com>
2024-08-26 14:56:53 -0300

    feat: added database connection test

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,10 +1,19 @@
 import 'dotenv/config';
 import app from './app';
+import { prisma } from './prisma/client';
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, async () => {
 	try {
 		console.log('server is running');
-	} catch (error) {}
+		await prisma.$connect();
+		console.log('Database connected');
+	} catch (error) {
+		console.log(
+			'Error connecting to database:',
+			JSON.stringify(error, null, '\t')
+		);
+		process.exit(1);
+	}
 });
