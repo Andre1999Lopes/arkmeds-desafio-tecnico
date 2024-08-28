b7b153cbebeaf0db7d66f3ef22af444db662fac9
Andre1999Lopes <andrelopesbr1999@gmail.com>
2024-08-28 15:05:58 -0300

    fix: remode unused logs

--- src/infra/queue/consumer/KafkaConsumer.ts
+++ src/infra/queue/consumer/KafkaConsumer.ts
@@ -42,8 +42,6 @@ export class KafkaConsumer {
 		await this.consumer.run({
 			eachMessage: async ({ message }) => {
 				const raceData = JSON.parse(message.value!.toString()) as MessageDTO;
-				console.log(raceData);
-				console.log(__dirname);
 				const receiptPath = path.join(
 					__dirname,
 					'../../../tmp',
